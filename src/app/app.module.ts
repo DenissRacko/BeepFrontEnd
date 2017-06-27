4ac07a4045d77045e18c26b033c9e3e595b5bf6e
Deniss <deniss.racko@inbox.lv>
2017-06-27 14:14:42 +0300

    Added some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdInputModule, MdSelectModule, MdSidenavModule } from '@angular/material';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '*', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(appRoutes),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdButtonModule, MdCheckboxModule, MdInputModule, MdSelectModule, MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
